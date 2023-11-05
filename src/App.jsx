8919379c96213c9e33780e1f9d3a2dac6a1264d5
Vimalesh17 <vimal7123@gmail.com>
2023-11-05 13:44:11 +0530

    form created

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,16 @@ function App() {
 
   return (
     <>
-      
+      <div className='container'>
+        <form className='form'>
+        <input type='text' className='input_field' placeholder='First Name'/>
+        <input type='text' className='input_field' placeholder='Last Name'/>
+        <input type='email' className='input_field' placeholder='Email'/>
+        <input type='password' className='input_field' placeholder='Password'/>
+        <input type='password' className='input_field' placeholder='confirm password'/>
+        <input type='submit' id='submit' className='button'/>
+        </form>
+      </div>
     </>
   )
 }
